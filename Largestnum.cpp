855c9e827a1fb29fd0d388ae079a6f0c02a7475e
NalandaPatil <71128762+NalandaPatil@users.noreply.github.com>
2020-11-20 00:39:04 +0530

    Create Largestnum.cpp

---
+++ Largestnum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2, num3;
+
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    if (num1 >= num2) {
+        if (num1 >= num3)
+            cout << "Largest number is " << num1;
+        else
+            cout << "Largest number is " << num3;
+    }
+    else {
+        if (num2 >= num3)
+            cout << "Largest number is " << num2;
+        else
+            cout << "Largest number is " << num3;
+    }
+
+    return 0;
+}
